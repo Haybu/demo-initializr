e509c6d72154aaa24fa4dbdd0b7e7e2612b64c10
Haytham Mohamed <hmohamed@Haythams-MBP-2.attlocal.net>
2018-04-12 00:05:07 -0500

    init

---
+++ src/main/java/io/agilehandy/initializr/MyInitializrApplication.java
@@ -0,0 +1,14 @@
+package io.agilehandy.initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+public class MyInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyInitializrApplication.class, args);
+	}
+}
